ce80617692baa4167c987cc595793cec9942e5b5
Maksim <tkachenkonino1996@gmail.com>
2021-08-25 22:08:36 +0300

    add aith page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { getDay } from './day';
-
-const day = document.querySelector('.today');
-day.innerHTML = getDay();
+// import Handlebars from 'handlebars/dist/handlebars.runtime';
+// import template from './button.hbs';
+import authPageTemplate from './pages/auth-page/auth-page.hbs';
+// const button = template({ buttonName: 'Кнопка' });
+const authPage = authPageTemplate();
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.innerHTML = authPage;
+});
