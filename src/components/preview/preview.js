fc21c90d65bc230df0c695a6b297170c512f813a
Maksim <tkachenkonino1996@gmail.com>
2021-08-26 22:18:30 +0300

    add chat page

---
+++ src/components/preview/preview.js
@@ -0,0 +1,3 @@
+import Handlebars from 'handlebars/dist/handlebars.runtime';
+import teplate from './preview.hbs';
+Handlebars.registerPartial('preview', teplate);
--- src/index.js
+++ src/index.js
@@ -2,15 +2,82 @@
 // import template from './button.hbs';
 import './components/input/input';
 import './components/button/button';
+import './components/preview/preview';
+
+const chats = [
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  }
+];
 
 // import authPageTemplate from './pages/auth-page/auth-page.hbs';
 import registerPageTemplate from './pages/register-page/register-page.hbs';
 import chatPageTemplate from './pages/chat-page/chat-page.hbs';
 
-// const button = template({ buttonName: 'Кнопка' });
 // const authPage = authPageTemplate();
 // const registerPage = registerPageTemplate();
-const chatPage = chatPageTemplate();
+const chatPage = chatPageTemplate({ chats });
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.innerHTML = chatPage;
