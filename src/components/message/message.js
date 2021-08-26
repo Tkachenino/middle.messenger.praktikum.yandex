68b747ba08a5e689a3bf3f297dd50d8929e5688c
Maks Tkachenko <tkachenkonino1996@gmail.com>
2021-08-26 23:32:01 +0300

    app profile

---
+++ src/components/message/message.js
@@ -0,0 +1,3 @@
+import Handlebars from 'handlebars/dist/handlebars.runtime';
+import teplate from './message.hbs';
+Handlebars.registerPartial('message', teplate);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,81 @@
-// import Handlebars from 'handlebars/dist/handlebars.runtime';
-// import template from './button.hbs';
 import './components/input/input';
 import './components/button/button';
 import './components/preview/preview';
+import './components/message/message';
 
 const chats = [
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
+  {
+    name: 'Влад',
+    image:
+      'https://images.unsplash.com/profile-1589352637695-cd91ccd020f0image?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    text: 'Text message',
+    date: '11:23',
+    count: '12'
+  },
   {
     name: 'Влад',
     image:
@@ -71,14 +142,28 @@ const chats = [
   }
 ];
 
-// import authPageTemplate from './pages/auth-page/auth-page.hbs';
+const messages = [
+  { text: 'GGdgdgdg', date: '10:20', own: false },
+  { text: 'GGdgdgdg', date: '10:20', own: false },
+  { text: 'GGdgdgdg', date: '10:20', own: true },
+  { text: 'GGdgdgdg', date: '10:20', own: false },
+  { text: 'GGdgdgdg', date: '10:20', own: false },
+  { text: 'GGdgdgdg', date: '10:20', own: true },
+  { text: 'GGdgdgdg', date: '10:20', own: false },
+  { text: 'GGdgdgdg', date: '10:20', own: false },
+  { text: 'GGdgdgdg', date: '10:20', own: true },
+  { text: 'GGdgdgdg', date: '10:20', own: false }
+];
+import authPageTemplate from './pages/auth-page/auth-page.hbs';
 import registerPageTemplate from './pages/register-page/register-page.hbs';
 import chatPageTemplate from './pages/chat-page/chat-page.hbs';
+import profilePageTemplate from './pages/profile-page/profile-page.hbs';
 
-// const authPage = authPageTemplate();
-// const registerPage = registerPageTemplate();
-const chatPage = chatPageTemplate({ chats });
+const authPage = authPageTemplate();
+const registerPage = registerPageTemplate();
+const chatPage = chatPageTemplate({ chats, messages });
+const profilePage = profilePageTemplate();
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.innerHTML = chatPage;
+  document.body.innerHTML = profilePage;
 });
