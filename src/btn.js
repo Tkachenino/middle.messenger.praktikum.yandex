25433d5f23f502db91977d67564beeb10debf57a
Maks Tkachenko <tkachenkonino1996@gmail.com>
2021-08-26 19:16:44 +0300

    add chat page

--- src/btn.js
+++
@@ -1,1 +0,0 @@
-import template from './button.hbs';
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import './components/button/button';
 
 // import authPageTemplate from './pages/auth-page/auth-page.hbs';
 import registerPageTemplate from './pages/register-page/register-page.hbs';
+import chatPageTemplate from './pages/chat-page/chat-page.hbs';
 
 // const button = template({ buttonName: 'Кнопка' });
 // const authPage = authPageTemplate();
-const registerPage = registerPageTemplate();
+// const registerPage = registerPageTemplate();
+const chatPage = chatPageTemplate();
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.innerHTML = registerPage;
+  document.body.innerHTML = chatPage;
 });
